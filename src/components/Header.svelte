c57b6981fa22b9e4a0c8f1bdbd6cf5e29dcb3f6a
Freddy <gay@fuck.com>
2022-01-17 12:40:29 +0300

    init

---
+++ src/components/Header.svelte
@@ -0,0 +1,36 @@
+<nav>
+    <ul>
+        <a href="/"><img src="" alt="logo"></a>
+        <li><a href="/news">Новости</a></li>
+        <li><a href="/articles">Статьи</a></li>
+        <li><a href="/products">Продукция</a></li>
+        <li><a href="/services">Услуги</a></li>
+        <li><a href="/contacts">Контакты</a></li>
+        <li><a href="about">О нас</a></li>
+    </ul>
+</nav>
+
+<style lang="scss">
+    @import "../base";
+
+    :global(#svelte, nav) {
+      width: 100vw;
+      height: 53px;
+    }
+
+    ul {
+        display: flex;
+        flex-flow: row;
+
+        list-style-type: none;
+
+        & > * {
+            padding-right: 10px;
+        }
+
+        @media (min-width: $mobile) {
+          background-color: red;
+        }
+    }
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+    :global(body) {
+      margin: 0;
+      padding: 0;
+    }
+
+    :global(*) {
+      box-sizing: border-box;
+    }
+</style>
