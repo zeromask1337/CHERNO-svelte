e729df04192e75cb8076f21ebd7d7994c1e2d671
Freddy <gay@fuck.com>
2022-01-18 17:02:12 +0300

    feat: header done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,13 +5,71 @@
 
 <p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
 
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p>
+
 <style lang="scss">
     :global(body) {
-      margin: 0;
-      padding: 0;
+      min-height: 100vh;
+
+      font-family: 'Montserrat', sans-serif;
+      color: black;
     }
 
     :global(*) {
+      margin: 0;
+      padding: 0;
       box-sizing: border-box;
+      text-decoration: none;
+      list-style-type: none;
+    }
+
+    :global(button) {
+      all: unset;
+    }
+
+    :global(a) {
+        color: inherit;
     }
 </style>
